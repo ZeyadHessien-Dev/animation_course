53e87f31e75a9ca1facf1a48d8c695fb88949b2a
ZeyaD HessieN <zeyad.hessien1123@gmail.com>
2022-09-11 18:09:14 +0200

    Rotation Transition Class

--- lib/modules/animation_route_transition/animation.dart
+++ lib/modules/animation_route_transition/animation.dart
@@ -8,18 +8,20 @@ class SlideRight extends PageRouteBuilder {
       : super(
           pageBuilder: (context, animation, animationTwo) => page,
           transitionsBuilder: (context, animation, animationTwo, child) {
-            var begin = const Offset(0.0, 1.0);
-              var end = const Offset(0.0, 0.0);
-              var tween = Tween(
-                begin: begin,
-                end: end,
-              );
-              var offsetAnimation = animation.drive(tween);
-            return SlideTransition(
-              position: offsetAnimation,
+            var begin = 1.0;
+            var end = 0.0;
+            var tween = Tween(
+              begin: begin,
+              end: end,
+            );
+            var curvedAnimation = CurvedAnimation(
+              parent: animation,
+              curve: Curves.linear ,
+            );
+            return RotationTransition(
+              turns: tween.animate(curvedAnimation),
               child: child,
             );
-
           },
         );
 }
