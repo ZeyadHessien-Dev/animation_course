78117fe52b773a73267451c002d3b9149aaf7f8c
ZeyaD HessieN <zeyad.hessien1123@gmail.com>
2022-09-11 18:26:02 +0200

    Size Transition Class And Align

--- lib/modules/animation_route_transition/animation.dart
+++ lib/modules/animation_route_transition/animation.dart
@@ -8,26 +8,13 @@ class SlideRight extends PageRouteBuilder {
       : super(
           pageBuilder: (context, animation, animationTwo) => page,
           transitionsBuilder: (context, animation, animationTwo, child) {
-            var begin = 1.0;
-              var end = 1.0;
-              var tween = Tween(
-                begin: begin,
-                end: end,
-              );
-
-            var curveAnimation = CurvedAnimation(
-              parent: animation,
-              curve: Curves.linear,
-            );
-            // return SlideTransition(
-            //   position: tween.animate(curveAnimation),
-            //   child: child,
-            // );
-
-            return ScaleTransition(
-              // Here Cancel Offset [it's use it just in SlideTransition]
-              scale: tween.animate(curveAnimation),
-              child: child,
+            return Align(
+              // Increase Size
+              alignment: AlignmentDirectional.topCenter,
+              child: SizeTransition(
+                sizeFactor: animation,
+                child: child,
+              ),
             );
           },
         );
