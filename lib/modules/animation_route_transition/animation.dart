a5a1e028c25cd2ebb7d54d6398e295d8be6afeb1
ZeyaD HessieN <zeyad.hessien1123@gmail.com>
2022-09-11 17:37:29 +0200

    Animation Route

--- lib/modules/animation_route_transition/animation.dart
+++ lib/modules/animation_route_transition/animation.dart
@@ -8,27 +8,18 @@ class SlideRight extends PageRouteBuilder {
       : super(
           pageBuilder: (context, animation, animationTwo) => page,
           transitionsBuilder: (context, animation, animationTwo, child) {
-            var begin = 1.0;
-              var end = 1.0;
+            var begin = const Offset(0.0, 1.0);
+              var end = const Offset(0.0, 0.0);
               var tween = Tween(
                 begin: begin,
                 end: end,
               );
-
-            var curveAnimation = CurvedAnimation(
-              parent: animation,
-              curve: Curves.linear,
-            );
-            // return SlideTransition(
-            //   position: tween.animate(curveAnimation),
-            //   child: child,
-            // );
-
-            return ScaleTransition(
-              // Here Cancel Offset [it's use it just in SlideTransition]
-              scale: tween.animate(curveAnimation),
+              var offsetAnimation = animation.drive(tween);
+            return SlideTransition(
+              position: offsetAnimation,
               child: child,
             );
+
           },
         );
 }
