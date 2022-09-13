9a1186572211460f8cf71bac30c56834cad12b48
Zeyad Hessien <112716677+ZeyadHessien-Dev@users.noreply.github.com>
2022-09-13 16:22:25 +0200

    Update animation.dart

--- lib/modules/animation_route_transition/animation.dart
+++ lib/modules/animation_route_transition/animation.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 
 // Here Classes To Control with transition route
-class SlideRight extends PageRouteBuilder {
+class TwoAnimate extends PageRouteBuilder {
   var page;
 
-  SlideRight({this.page})
+  TwoAnimate({this.page})
       : super(
-
     pageBuilder: (context, animation, animationTwo) => page,
     transitionsBuilder: (context, animation, animationTwo, child) {
       var begin = 0.9;
@@ -30,24 +29,4 @@ class SlideRight extends PageRouteBuilder {
       );
     },
   );
-
-          pageBuilder: (context, animation, animationTwo) => page,
-          transitionsBuilder: (context, animation, animationTwo, child) {
-            var begin = 1.0;
-            var end = 0.0;
-            var tween = Tween(
-              begin: begin,
-              end: end,
-            );
-            var curvedAnimation = CurvedAnimation(
-              parent: animation,
-              curve: Curves.linear ,
-            );
-            return RotationTransition(
-              turns: tween.animate(curvedAnimation),
-              child: child,
-            );
-          },
-        );
-
 }
