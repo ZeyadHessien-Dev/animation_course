1a7a4eceb7f81a7485306c97ac61176ec6dcaaef
Zeyad Hessien <112716677+ZeyadHessien-Dev@users.noreply.github.com>
2022-09-13 16:47:50 +0200

    Update animation.dart

--- lib/modules/animation_route_transition/animation.dart
+++ lib/modules/animation_route_transition/animation.dart
@@ -1,24 +1,34 @@
 import 'package:flutter/material.dart';
 
-// Here Classes To Control with transition route
-class SlideRight extends PageRouteBuilder {
-  var page;
+class TwoAnimate extends PageRouteBuilder {
+  dynamic page;
 
-  SlideRight({this.page})
+  TwoAnimate({this.page})
       : super(
-          pageBuilder: (context, animation, animationTwo) => page,
-          transitionsBuilder: (context, animation, animationTwo, child) {
-            var begin = const Offset(1.0, 0.0);
-            var end = const Offset(0.0, 0.0);
-            var tween = Tween(
-              begin: begin,
-              end: end,
-            );
-            var offsetAnimation = animation.drive(tween);
-            return SlideTransition(
-              position: offsetAnimation,
-              child: child,
-            );
-          },
-        );
+            pageBuilder: (context, animation, animationTwo) => page,
+            transitionsBuilder: (context, animation, animationTwo, child) {
+              var begin = 1.0;
+              var end = 1.0;
+              var curve = Curves.ease;
+              var tween = Tween(
+                begin: begin,
+                end: end,
+              ).chain(CurveTween(curve: curve));
+              var curvedAnimation = CurvedAnimation(
+                parent: animation,
+                curve: Curves.elasticIn,
+              );
+              return ScaleTransition(
+                scale: tween.animate(
+                  curvedAnimation,
+                ),
+                child: Align(
+                  alignment: AlignmentDirectional.bottomEnd,
+                  child: SizeTransition(
+                    sizeFactor: animation,
+                    child: child,
+                  ),
+                ),
+              );
+            });
 }
